bdac2bec95b85910b56850c688bb75a968ad7e44
cd <lesleyrotonto@gmail.com>
2023-12-01 16:21:33 -0500

    some changes

--- app.js
+++ app.js
@@ -4,6 +4,16 @@
 let coanvas = document.getElementById('canvas')
 let context = canvas.getContext('2d')
 
+//event handlers
+window.addEventListener('keydown', e =>{
+    switch (e.key){
+        case 'ArrowUp':
+        inputDirection = {x: 0, y: -1}
+        case 'ArrowDown'
+        inputDirection = {x: 0, y: 1}
+    }
+})
+
 //window 
 const windowHeight = window.innerHeight
 const windowWidth = window.innerWidth
@@ -16,5 +26,4 @@ canvas.style.background = 'black'
 context.fillStyle = 'white'
 let paddleOne = context.fillRect(0, 0, 50, 200)
 let paddleTwo = context.fillRect(685,551,50,200)
-
 
