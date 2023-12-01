9dd43fd24b2858ce680c3a04bf44195f0ec2625a
cd <lesleyrotonto@gmail.com>
2023-12-01 15:47:22 -0500

    more basics

--- app.js
+++ app.js
@@ -10,12 +10,11 @@ const windowWidth = window.innerWidth
 
 canvas.height = windowHeight
 canvas.width = windowWidth
-
 canvas.style.background = 'black'
 
 //paddles
 context.fillStyle = 'white'
 let paddleOne = context.fillRect(0, 0, 50, 200)
-let paddleTwo =context.fillRect(685,551,50,200)
+let paddleTwo = context.fillRect(685,551,50,200)
 
 
