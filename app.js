9c8ab653e6c0588edc0db4f62402e73f0790a05f
cd <lesleyrotonto@gmail.com>
2023-12-01 15:38:32 -0500

    some changes

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+
+
+// cached
 let coanvas = document.getElementById('canvas')
 let context = canvas.getContext('2d')
 
-const windowHeight = 600
-const windowWidth = 700
+//window 
+const windowHeight = window.innerHeight
+const windowWidth = window.innerWidth
 
 canvas.height = windowHeight
 canvas.width = windowWidth
 
 canvas.style.background = 'black'
+
+//paddles
+context.fillStyle = 'white'
+let paddleOne = context.fillRect(0, 0, 50, 200)
+let paddleTwo =context.fillRect(685,551,50,200)
+
+
