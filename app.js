87fd46f6b7a6599dbde408a3ee26c9b5f503c6be
cd <lesleyrotonto@gmail.com>
2023-12-01 19:48:06 -0500

    tinny changes

--- app.js
+++ app.js
@@ -8,9 +8,17 @@ let context = canvas.getContext('2d')
 window.addEventListener('keydown', e =>{
     switch (e.key){
         case 'ArrowUp':
-        inputDirection = {x: 0, y: -1}
-        case 'ArrowDown'
-        inputDirection = {x: 0, y: 1}
+            inputDirection = {x: 0, y: -1} 
+            break
+        case 'ArrowDown':
+            inputDirection = {x: 0, y: 1} 
+            break
+        case 'ArrowUp':
+            inputDirection = {x: 0, y: -1} 
+            break
+        case 'ArrowDown':
+            inputDirection = {x: 0, y: 1}
+            break
     }
 })
 
@@ -22,8 +30,18 @@ canvas.height = windowHeight
 canvas.width = windowWidth
 canvas.style.background = 'black'
 
-//paddles
+//paddles and ball
 context.fillStyle = 'white'
 let paddleOne = context.fillRect(0, 0, 50, 200)
-let paddleTwo = context.fillRect(685,551,50,200)
+let paddleTwo = context.fillRect(685,550,50,215)
 
+//might need to make a class out of this or function will come back
+context.beginPath()
+context.lineWidth = 5
+context.arc(200, 300, 10, 0 , Math.PI*2, false)
+context.strokeStyle = 'pink'
+context.stroke()
+context.closePath()
+
+
+//
