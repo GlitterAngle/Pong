955f4ffce7ad4bf68b32be0be55fedd8ffaa207a
cd <lesleyrotonto@gmail.com>
2023-12-03 15:14:01 -0500

    there is automated ball movement

--- app.js
+++ app.js
@@ -16,12 +16,7 @@ canvas.style.height = (canvas.height / 2) + 'px'
 
 canvas.style.background = 'black'
 
-//paddles and ball
-// context.fillStyle = 'pink'
-// let paddleOne = context.fillRect(canvas.width/2 -700 ,canvas.
-// height/2 - 500,50,200)
-// context.fillStyle = 'blue'
-// let paddleTwo = context.fillRect(canvas.width/2 + 650, canvas.height/2 + 300, 50, 200)
+
 
 class Paddle{
     constructor(xpos,ypos,wpos,hpos, color, isPaddleOne){
@@ -47,8 +42,22 @@ class Circle{
         this.radius = radius
         this.color = color
         this.speed = speed
-        this.dx = 1 + this.speed
-        this.dy = 1 + this.speed
+        this.dx = 1 
+        this.dy = 1 
+    }
+    randomStart(){
+        if(Math.floor(Math.random()* 2) == 1){
+            this.dx = 1
+        } else {
+            this.dx = -1
+        }
+        if(Math.floor(Math.random()* 2) == 1){
+            this.dy = 1
+        } else {
+            this.dy = -1
+        }
+        this.xpos = canvas.width/2
+        this.ypos = canvas.height/2
     }
     draw(context) {
         context.beginPath()
@@ -57,25 +66,38 @@ class Circle{
         context.fillStyle = this.color
         context.fill()
         context.stroke()
-        context.closePath()   
+        context.closePath()
     }
     bounce(){
-        this.draw(context)
+        this.draw(context) 
+        
+        if (this.ypos <= 0 + this.radius){
+            this.dy *= -1
+        }
+        if(this.ypos >= canvas.height - this.radius){
+            this.dy *= -1
+        }
+        //bounce off paddle
+        if(this.xpos <= (paddleOne.xpos + paddleOne.wpos +this.radius)){
+            if(this.ypos > paddleOne.ypos && this.ypos < paddleOne.ypos + paddleOne.hpos){
+                this.dx *= -1
+            }
+        } 
+        if(this.xpos >= (paddleTwo.xpos - this.radius)){
+            if(this.ypos > paddleTwo.ypos && this.ypos < paddleTwo.ypos + paddleTwo.hpos){
+                this.dx *= -1
+            }
+        } 
 
-        if ((this.ypos + this.radius) > canvas.height){//bottom
-            this.dy = -this.dy
-           }
-           if((this.ypos - this.radius)< 0){ //top
-            this.dy = -this.dy
-           }
 
-        this.xpos += this.dx
-        this.ypos += this.dy
+        this.xpos += (this.speed*this.dx)
+        this.ypos += (this.speed*this.dy)
     }
-
+   
 }
 
-let pong = new Circle(canvas.width/2, canvas.height/2, 20, 'white', 5)
+let pong = new Circle(canvas.width/2, canvas.height/2, 20, 'white', 10)
+pong.randomStart()
 let paddleOne = new Paddle(canvas.width, canvas.height, 50, 200,'pink', true)
 let paddleTwo = new Paddle(canvas.width, canvas.height,50,200, 'blue', false)
 
@@ -88,7 +110,8 @@ let currentTwo = paddleTwo.getCurrentY()
     if(e.keyCode === 38){
         if(currentTwo > 0 ){
             currentTwo -= speed
-            paddleTwo.ypos = currentTwo 
+            paddleTwo.ypos = currentTwo
+            pong.speed = 10 
 
             
         }
@@ -97,18 +120,21 @@ let currentTwo = paddleTwo.getCurrentY()
         if(currentTwo + paddleTwo.hpos < canvas.height){
             currentTwo += speed
             paddleTwo.ypos = currentTwo
+            pong.speed = 10
         }
     }
     if(e.keyCode === 87){
         if(currentOne > 0){
             currentOne -= speed
             paddleOne.ypos = currentOne
+            pong.speed = 10
         }
     }
     if(e.keyCode === 83){
         if(currentOne + paddleOne.hpos < canvas.height){
             currentOne += speed
             paddleOne.ypos = currentOne
+            pong.speed = 10
         }
     }
 
@@ -131,8 +157,8 @@ function updateBoard(){
     paddleTwo.create(context);
     requestAnimationFrame(updateBoard)
     pong.bounce()
+    pong.collision()
     pong.draw(context)
-
 }
 
 
