8af72c9f28f1ea7f8614e7f18ff5e6c5d8f342fb
cd <lesleyrotonto@gmail.com>
2023-12-08 10:24:37 -0500

    sound now pauses

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
+<<<<<<< HEAD
+# Pong
+=======
+# Project Proposal : Pong
+
+## Project Description
+This is a recreation of the classic game from the 70s. Two players can play, whoever reaches a score of 5 first wins. Users will have the option to play again. In the future, I would like to add a feature where there is an automated player and a shrinking paddle as the game progresses, and a pause button for the musice and sound effects (for better accessibility)
+
+## Technologies Used
+I used HTML CSS and JavaScript to complete my game!
+
+## Wire Frames 
+Initial landing view
+<img width="989" alt="Screenshot 2023-11-30 at 8 33 26 PM" src="https://github.com/GlitterAngle/Pong/assets/138747127/3af39682-b966-4dfa-9694-d99650aef481">
+
+Results 
+<img width="985" alt="Screenshot 2023-11-30 at 8 31 07 PM" src="https://github.com/GlitterAngle/Pong/assets/138747127/44e71e32-991e-48cb-81da-5d120be217a2">
+
+
+
+#### MVP Goals 
+
+- As a player I want my game to recognize when the ball is off screen so a winner is declared. 
+- As a player, I would like to be informed when the game is over.
+- As a player for those who require assistive technolog ies,I would like accessibility features so that others are not left out of enjoying the game.
+- As a player, I would like to be able to restart the game after a round is done.
+- As a player, I would like the UI to be engaging and easy on the eyes
+
+#### Stretch Goals
+
+- As a player, I would like to hear a little chime when the ball hits a paddle. Completed 
+- As a player, I would like the game to increase in difficulty with a smaller paddle after every 5 strikes. not completed 
+- As a player, I would like to have the option to play against the computer. Not completed
+
+## Pseudocode 
+
+- Define the required variables used to track the state of the game(ball position, paddle positions, scores)
+- Store cached element references (Reset Buttone, Winner Message, Game Board, Paddle 1, Paddle 2, Ball)
+- Event Handlers (Key clicks for player 1, Key clicks for player 2)
+- Game Logic (Update Board and Update Message)
+- Function to render game (Call functions to update the board, update the message)
+- Function to edge (keep game elements within the board boundaries)
+- function to move the ball
+- Function to bounce the ball off paddles and walls (only top and bottom walls)
+- Function to determin a winner 
+- Function to update the board (Upon load the page will have a set position and not being until a key click)
+ 
+
+>>>>>>> 87dde38560507c003575dae0189c21caae1c17b2
 
 
 
---
+++ makeMusicPause.js
@@ -0,0 +1,333 @@
+// cached
+let canvas = document.getElementById('canvas')
+let context = canvas.getContext('2d')
+let pausePlay = document.getElementById('toggleMusic')
+const msg = document.getElementById('msg')
+const reset = document.querySelector('button')
+const audioContext = new (window.AudioContext || window.AudioContext)();
+const backgroundMusic = new Audio();
+
+// backgroundMusic.src = 'asset/bit-shift-kevin-macleod-main-version-24901-03-12.mp3';
+// backgroundMusic.loop = true;
+// backgroundMusic.autoplay = false;
+
+
+
+
+let scoreSound = new Audio()
+scoreSound.src = 'asset/notification-positive-bleep-joshua-chivers-1-00-01.mp3'
+//window 
+
+canvas.width = 1800
+canvas.height = 1000
+
+canvas.style.width = (canvas.width / 2) + 'px'
+canvas.style.height = (canvas.height / 2) + 'px'
+
+canvas.style.background = 'black'
+
+
+
+//pieces 
+class Score{
+    constructor(num, xpos, ypos, color){
+        this.num = num
+        this.xpos = xpos
+        this.ypos = ypos
+        this.color = color
+    }
+    write(){
+        context.fillStyle= this.color
+        context.font = '50px Arial'
+        context.fillText(this.num, this.xpos, this.ypos)
+    }
+}
+
+class Paddle{
+    constructor(xpos,ypos,wpos,hpos, color, speed, isPaddleOne){
+        this.xpos = isPaddleOne ? xpos/ 2 - 900 : xpos /2 + 850
+        this.ypos = isPaddleOne ? ypos/2 : ypos/2 
+        this.wpos = wpos
+        this.hpos = hpos
+        this.color = color
+        this.speed = speed
+    }
+    create(context) {
+        context.fillStyle = this.color
+        context.fillRect(this.xpos,this.ypos,this.wpos,this.hpos)
+    }
+   getCurrentY(){
+    return this.ypos
+   }
+}
+
+class Circle{
+    constructor(xpos, ypos, radius, color, velocity){
+        this.xpos = xpos
+        this.ypos = ypos
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+        this.dx = 1 
+        this.dy = 1 
+    }
+    draw(context) {
+        context.beginPath()
+        context.lineWidth = 5
+        context.arc(this.xpos, this.ypos, this.radius, 0 , Math.PI*2, false)
+        context.fillStyle = this.color
+        context.fill()
+        context.stroke()
+        context.closePath()
+    }
+    
+    enabledBounce = true
+
+    bounce(){ // creates movement of the ball 
+        this.draw(context) 
+
+        let sound = new Audio()
+        sound.src = 'asset/pongSound_01.wav'
+        
+        if (this.ypos <= 0 + this.radius || this.ypos >= canvas.height - this.radius){
+            this.dy *= -1
+            sound.play()
+        }
+        this.xpos += this.dx * this.velocity
+        this.ypos += this.dy * this.velocity
+        //bounce off paddle
+        if(this.xpos <= (paddleOne.xpos + paddleOne.wpos +this.radius)){
+            if (!this.enabledBounce){
+                return}
+            if(this.ypos > paddleOne.ypos - this.radius && this.ypos < paddleOne.ypos + paddleOne.hpos + this.radius){
+                this.dx *= -1
+                this.velocity ++
+                paddleOne.speed ++
+                paddleTwo.speed ++
+                this.enabledBounce = false
+                setTimeout(() => {
+                    this.enabledBounce = true
+                }, 250)
+                sound.play();
+            }
+        } 
+        if(this.xpos >= (paddleTwo.xpos - this.radius)){
+            if (!this.enabledBounce){
+                return}
+            if(this.ypos > paddleTwo.ypos - this.radius && this.ypos < paddleTwo.ypos + paddleTwo.hpos + this.radius){
+                this.dx *= -1
+                this.velocity ++
+                paddleOne.speed ++
+                paddleTwo.speed ++
+                this.enabledBounce = false
+                setTimeout(() => {
+                    this.enabledBounce = true
+                }, 250)
+                sound.play();
+                
+            }
+        } 
+    }
+    randomStart(){ //makes the ball go in a random direction 
+        
+        if(Math.floor(Math.random()* 2) == 1){
+            this.dx = 1
+        } else {
+            this.dx = -1
+        }
+        if(Math.floor(Math.random()* 2) == 1){
+            this.dy = 1
+        } else {
+            this.dy = -1
+        }
+        this.xpos = canvas.width/2
+        this.ypos = canvas.height/2
+    }
+    checkOutOfBounds(){
+        if(this.xpos < 0 || this.xpos > canvas.width){
+            scoreSound.play()
+            this.randomStart()
+        }
+    }
+}
+
+let pong = new Circle(canvas.width/2, canvas.height/2, 20, '#5ffbf1', 10)
+
+let paddleOne = new Paddle(canvas.width, canvas.height, 60, 200,'#5ffbf1', 60, true)
+let paddleTwo = new Paddle(canvas.width, canvas.height,60,200, '#5ffbf1', 60, false)
+
+let scoreTwo = new Score( 0 , canvas.width - 250,canvas.height - 850, '#5ffbf1')
+let scoreOne = new Score( 0, canvas.width - 1599, canvas.height - 850, '#5ffbf1')
+
+
+
+//event listeners
+
+window.addEventListener('keydown', (e) => {
+    let currentOne = paddleOne.getCurrentY()
+    let currentTwo = paddleTwo.getCurrentY()
+
+    switch (e.keyCode) {
+        case 38:  
+            if (currentTwo > 0) {
+                paddleTwo.ypos = Math.max(paddleTwo.ypos - paddleTwo.speed, 0)
+            }
+            break
+        case 40:  
+            if (currentTwo + paddleTwo.hpos < canvas.height) {
+                paddleTwo.ypos += paddleTwo.speed
+            }
+            break
+        case 87:  
+            if (currentOne > 0) {
+                paddleOne.ypos = Math.max(paddleOne.ypos - paddleOne.speed, 0)
+            }
+            break
+        case 83:  
+            if (currentOne + paddleOne.hpos < canvas.height) {
+                paddleOne.ypos += paddleOne.speed
+            }
+            break
+    }
+
+    
+})
+
+
+
+reset.addEventListener('click', resetGame)
+
+pause.addEventListener('click', function(){
+
+    backgroundMusic.src = 'asset/bit-shift-kevin-macleod-main-version-24901-03-12.mp3';
+backgroundMusic.loop = true;
+backgroundMusic.autoplay = false;
+    if (backgroundMusic.autoplay === true){
+        backgroundMusic.pause()
+        return backgroundMusic.autoplay = false
+    }
+    if(backgroundMusic.autoplay === false){
+        backgroundMusic.play()
+       return backgroundMusic.autoplay = true
+    }
+})
+
+function updateScoreOne(value){
+    let sum = value
+    if (scoreOne && pong.xpos > canvas.width){
+        sum++
+        return scoreOne.num = sum
+    }
+}
+function playerOneName(){
+    context.fillStyle = '#5ffbf1'
+    context.font = '50px Courier New'
+    context.fillText('Player One', canvas.width/2 - 860 
+    , canvas.height/2 - 450 )
+}
+function updateScoreTwo(value){
+    let sum = value
+    if(scoreTwo && pong.xpos < 0){
+        sum++
+        return scoreTwo.num = sum
+    }
+}
+
+function playerTwoName(){
+    context.fillStyle = '#5ffbf1'
+    context.font = '50px Courier New'
+    context.fillText('Player Two', canvas.width - 400 
+    , canvas.height/2 - 450 )
+}
+
+
+//functions 
+
+function resetGame() {
+    scoreOne.num = 0
+    scoreTwo.num = 0
+
+    paddleOne.ypos = canvas.height / 2;
+    paddleTwo.ypos = canvas.height / 2;
+
+    pong.xpos = canvas.width / 2;
+    pong.ypos = canvas.height / 2;
+    pong.velocity = 10;
+
+    pong.dx = 1;
+    pong.dy = 1;
+    paddleOne.speed = 60;
+    paddleTwo.speed = 60;
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+     pong.randomStart() 
+}
+
+
+
+function stopGame(){
+    pong.velocity= 0
+    paddleOne.speed = 0
+    paddleTwo.speed = 0
+    backgroundMusic.pause()
+    backgroundMusic.currentTime = 0
+}
+
+function checkForWinner(scoreOne,scoreTwo){
+    if(scoreOne.num === 5){
+        context.fillStyle= '#d16ba5'
+        context.font = '100px Courier New'
+        context.fillText ('Player One Wins!',canvas.width/2 - 450 , canvas.height/2 - 100)
+        stopGame()
+    }
+    if(scoreTwo.num === 5){
+        context.fillStyle= '#d16ba5'
+        context.font = '100px Courier New'
+        context.fillText ('Player Two Wins!', canvas.width/2 - 450, canvas.height/2 - 100)
+        stopGame()
+    }
+    
+}
+
+
+function render() {
+    pong.randomStart()
+    updateBoard()
+}
+
+
+function updateBoard(){
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    paddleOne.create(context)
+    paddleTwo.create(context)
+    scoreOne.write()
+    playerOneName()
+    scoreTwo.write()
+    playerTwoName()
+    backgroundMusic.play()
+    requestAnimationFrame(updateBoard)
+    pong.bounce()
+    checkForWinner(scoreOne,scoreTwo)
+    updateScoreOne(scoreOne.num)
+    updateScoreTwo(scoreTwo.num)
+    pong.checkOutOfBounds()
+    pong.draw(context)  
+}
+
+context.fillStyle = '#d16ba5'
+    context.font = '100px Courier New'
+    context.fillText('Press Space To Play !', canvas.width/2 - 600 , canvas.height/2 - 300 )
+    context.font = '60px Courier New'
+    context.fillText('Player One to move the right paddle', canvas.width/2 - 700, canvas.height/2 - 100 )
+    context.fillText('press W to go up and S to go down', canvas.width/2 - 700, canvas.height/2)
+    context.fillText('Player Two to move the left paddle press', canvas.width/2 - 700, canvas.height/2 + 200)
+    context.fillText('Up Arrow to go up and Down Arrow to', canvas.width/2 - 700, canvas.height/2 + 300)
+    context.fillText('go down', canvas.width/2 - 700, canvas.height/2 + 400)
+
+window.addEventListener('keydown', function(e){
+    if (e.keyCode === 32 ){
+    render()}
+})
+
+start()
